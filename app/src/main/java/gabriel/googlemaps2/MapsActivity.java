734b869d7630b80117135f1f9e46b95e6fe297f0
gabojm <gabojarmuaz@gmail.com>
2019-12-15 21:07:48 -0400

    Images readme

--- app/src/main/java/gabriel/googlemaps2/MapsActivity.java
+++ app/src/main/java/gabriel/googlemaps2/MapsActivity.java
@@ -41,31 +41,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         mMap.getUiSettings().setZoomControlsEnabled(true);
 
-        // Add a marker in Sydney and move the camera
         LatLng hernandoSiles = new LatLng(-16.499456, -68.123275);
         mMap.addMarker(new MarkerOptions().position(hernandoSiles).title("Estadio Hernando Siles"));
 
-        // Add a marker in Sydney and move the camera
         LatLng bombonera = new LatLng(-34.635634, -58.364896);
         mMap.addMarker(new MarkerOptions().position(bombonera).title("La Bombonera"));
 
-        // Add a marker in Sydney and move the camera
         LatLng maracana = new LatLng(-22.911664, -43.230810);
         mMap.addMarker(new MarkerOptions().position(maracana).title("Estadio Maracaná"));
 
-        // Add a marker in Sydney and move the camera
         LatLng campNou = new LatLng(41.380960, 2.122562);
         mMap.addMarker(new MarkerOptions().position(campNou).title("Estadio Camp Nou"));
 
-        // Add a marker in Sydney and move the camera
         LatLng parcPrinces = new LatLng(48.841323, 2.252770);
         mMap.addMarker(new MarkerOptions().position(parcPrinces).title("Estadio Parc de Princes"));
 
-        // Add a marker in Sydney and move the camera
         LatLng sanSiro = new LatLng(45.477935, 9.123992);
         mMap.addMarker(new MarkerOptions().position(sanSiro).title("Estadio San Siro"));
 
-        // Add a marker in Sydney and move the camera
         LatLng wembley = new LatLng(51.555763, -0.279635);
         mMap.addMarker(new MarkerOptions().position(wembley).title("Estadio de Wembley"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(wembley));
